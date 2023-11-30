e3aa65a53f22f3c41f989880d51d408010457e32
Seensh PC <feelipe.barbosa001@gmail.com>
2023-11-30 17:16:35 -0300

    requisições get all e put incluidas, +DTO

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -20,8 +20,9 @@ public class Medico {
 
     private String nome;
     private String email;
-    private String crm;
 
+    private String telefone;
+    private String crm;
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
 
@@ -34,5 +35,18 @@ public class Medico {
         this.crm = dados.crm();
         this.endereco = new Endereco(dados.endereco());
         this.especialidade = dados.especialidade();
+        this.telefone = dados.telefone();
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        if(dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null){
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
     }
 }
